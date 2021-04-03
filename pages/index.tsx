08312391f466097359e69c6a27d06399b982778c
sangwonyu <tkddnjs231@naver.com>
2021-04-03 22:06:26 +0900

    데이터 가져와서 뿌려보기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,17 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import ItemList from '../src/components/home/itemList';
 
 export default function Home() {
   const [text, setText] = useState<string>('typesciprt Test');
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>{text}</main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <ItemList />
     </div>
   );
 }
