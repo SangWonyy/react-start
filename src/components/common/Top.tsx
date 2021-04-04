1ff7911fe5b8e27db8d4f9d958a34752a41be29f
sangwonyu <tkddnjs231@naver.com>
2021-04-04 10:12:32 +0900

    공통 styled-component 진행중

--- src/components/common/Top.tsx
+++ src/components/common/Top.tsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
+import CommonDiv from '../../styles/component/common';
 
 export default function Top() {
   const TopStyle = styled.div`
     background: green;
   `;
   return (
-    <div>
+    <CommonDiv>
       <TopStyle>나는 Top</TopStyle>
-    </div>
+    </CommonDiv>
   );
 }
---
+++ src/styles/component/common.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default function CommonDiv() {
+  const CommonDivBox = styled.div`
+    margin-left: 40px;
+    margin-right: 40px;
+  `;
+
+  return <CommonDivBox></CommonDivBox>;
+}
